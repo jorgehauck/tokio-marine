87a727604602dba468facec68984d7a61b50bf75
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2025-03-19 11:05:31 -0300

    Refactor: Criação de classe de configuração para gerenciamento de Beans específicos.

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/ProvaTokioApplication.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/ProvaTokioApplication.java
@@ -11,8 +11,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
 @EnableSwagger2
 @SpringBootApplication
 public class ProvaTokioApplication {
@@ -20,18 +18,6 @@ public class ProvaTokioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaTokioApplication.class, args);
 	}
-	
-	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
-    }
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+
 
 }
