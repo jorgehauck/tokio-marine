f93eae12eef0a64684246b71b9131e5482d0e86d
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2025-03-22 18:42:04 -0300

    Refactor: Remoção de imports desnecessários

--- prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/ProvaTokioApplication.java
+++ prova-tokio-master-997744e0164edee6e74421275e5c167b326c7350/api-java/src/main/java/br/com/tokiomarine/seguradora/ProvaTokioApplication.java
@@ -2,22 +2,11 @@ package br.com.tokiomarine.seguradora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class ProvaTokioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaTokioApplication.class, args);
 	}
-
-
 }
